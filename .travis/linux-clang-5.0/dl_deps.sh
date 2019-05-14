e94cf532435a522c64e55cb88a3d8616a703f532
Syl Eze <syleze.contact@gmail.com>
2019-05-14 13:28:14 +0200

    Travis CI: fix permission denied when installing cmake 3.11

--- .travis/linux-clang-5.0/dl_deps.sh
+++ .travis/linux-clang-5.0/dl_deps.sh
@@ -2,4 +2,4 @@
 
 wget -O cmake3.11 https://cmake.org/files/v3.11/cmake-3.11.0-Linux-x86_64.tar.gz
 tar xvf cmake3.11
-cp -rf cmake-3.11.0-Linux-x86_64/bin/* /usr/bin/
+sudo cp -rf cmake-3.11.0-Linux-x86_64/bin/* /usr/bin/
